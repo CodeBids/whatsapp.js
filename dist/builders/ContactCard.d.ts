5d577815c09eade33c06d30abc99e26c35cae2a0
Julián <56706662+TheJulianJara@users.noreply.github.com>
2025-03-31 15:22:26 -0300

    Updated Card Class Functions

--- dist/builders/ContactCard.d.ts
+++ dist/builders/ContactCard.d.ts
@@ -15,4 +15,54 @@ export declare class ContactCard implements ContactInterface {
     userId?: string;
     urls?: WebSite[];
     constructor(data: ContactInterface);
+    /**
+     * Sets the addresses for the contact.
+     * @param addresses Array of addresses to be set.
+     */
+    setAddress(addresses: Adress[]): void;
+    /**
+     * Sets the birthday for the contact.
+     * @param birthday Date object representing the contact's birthday.
+     */
+    setBirthday(birthday: Date): void;
+    /**
+     * Sets the country information for the contact.
+     * @param country Country object representing the contact's country.
+     */
+    setCountry(country: Country): void;
+    /**
+     * Sets the company information for the contact.
+     * @param company Company object containing the company details.
+     */
+    setCompany(company: Company): void;
+    /**
+     * Sets the email addresses for the contact.
+     * @param emails Array of email objects to be set.
+     */
+    setEmail(emails: Email[]): void;
+    /**
+     * Sets the job information for the contact.
+     * @param job Job object containing the job title and department.
+     */
+    setJob(job: Job): void;
+    /**
+     * Sets the phone numbers for the contact.
+     * @param phones Array of phone objects to be set.
+     */
+    setPhone(phones: Phone[]): void;
+    /**
+     * Sets the name prefix for the contact.
+     * @param prefix String representing the name prefix.
+     */
+    setNamePrefix(prefix: string): void;
+    /**
+     * Sets the user ID for the contact.
+     * @param userId String representing the user ID.
+     */
+    setUserId(userId: string): void;
+    /**
+     * Sets the URLs associated with the contact.
+     * @param urls Array of website objects to be set.
+     */
+    setUrl(urls: WebSite[]): void;
 }
--- src/builders/ContactCard.ts
+++ src/builders/ContactCard.ts
@@ -1,22 +1,102 @@
 import { Adress, Country, Company, Email, Job, Phone, ContactCardData as ContactInterface, WebSite } from "../types";
 
 export class ContactCard implements ContactInterface {
-    addresses?: Adress[];
-    birthday?: Date;
-    country?: Country;
-    company?: Company;
-    emails?: Email[];
-    firstName!: string;
-    middleName?: string;
-    lastName?: string;
-    formattedName?: string;
-    job?: Job;
-    phones!: Phone[];
-    namePrefix?: string;
-    userId?: string;
-    urls?: WebSite[];
+  addresses?: Adress[];
+  birthday?: Date;
+  country?: Country;
+  company?: Company;
+  emails?: Email[];
+  firstName!: string;
+  middleName?: string;
+  lastName?: string;
+  formattedName?: string;
+  job?: Job;
+  phones!: Phone[];
+  namePrefix?: string;
+  userId?: string;
+  urls?: WebSite[];
 
   constructor(data: ContactInterface) {
-    Object.assign(this, data)
+    Object.assign(this, data);
+  }
+
+  /**
+   * Sets the addresses for the contact.
+   * @param addresses Array of addresses to be set.
+   */
+  public setAddress(addresses: Adress[]): void {
+    this.addresses = addresses;
+  }
+
+  /**
+   * Sets the birthday for the contact.
+   * @param birthday Date object representing the contact's birthday.
+   */
+  public setBirthday(birthday: Date): void {
+    this.birthday = birthday;
+  }
+
+  /**
+   * Sets the country information for the contact.
+   * @param country Country object representing the contact's country.
+   */
+  public setCountry(country: Country): void {
+    this.country = country;
+  }
+
+  /**
+   * Sets the company information for the contact.
+   * @param company Company object containing the company details.
+   */
+  public setCompany(company: Company): void {
+    this.company = company;
+  }
+
+  /**
+   * Sets the email addresses for the contact.
+   * @param emails Array of email objects to be set.
+   */
+  public setEmail(emails: Email[]): void {
+    this.emails = emails;
+  }
+
+  /**
+   * Sets the job information for the contact.
+   * @param job Job object containing the job title and department.
+   */
+  public setJob(job: Job): void {
+    this.job = job;
+  }
+
+  /**
+   * Sets the phone numbers for the contact.
+   * @param phones Array of phone objects to be set.
+   */
+  public setPhone(phones: Phone[]): void {
+    this.phones = phones;
+  }
+
+  /**
+   * Sets the name prefix for the contact.
+   * @param prefix String representing the name prefix.
+   */
+  public setNamePrefix(prefix: string): void {
+    this.namePrefix = prefix;
+  }
+
+  /**
+   * Sets the user ID for the contact.
+   * @param userId String representing the user ID.
+   */
+  public setUserId(userId: string): void {
+    this.userId = userId;
+  }
+
+  /**
+   * Sets the URLs associated with the contact.
+   * @param urls Array of website objects to be set.
+   */
+  public setUrl(urls: WebSite[]): void {
+    this.urls = urls;
   }
 }
