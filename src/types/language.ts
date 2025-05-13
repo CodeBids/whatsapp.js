a49fbc3dbed082c74a9feaf10403f23b2d76ee01
Julián <56706662+TheJulianJara@users.noreply.github.com>
2025-05-13 17:59:07 -0300

    Update language.ts

--- src/types/language.ts
+++ src/types/language.ts
@@ -1,4 +1,13 @@
+/**
+ * Enum for supported languages
+ * @reference https://developers.facebook.com/docs/whatsapp/api/messages/templates#supported-languages
+ */
+
 export enum LanguageCode {
+af = 'af',
+sq = 'sq',
+ar = 'ar',
 es = 'es',
+es_AR = 'es_AR',
 en_US = 'en_US'
 }
